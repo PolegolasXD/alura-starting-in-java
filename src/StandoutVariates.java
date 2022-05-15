ff98337a330db4fe7fb0a0102bef3e573e0ac53c
PolegolasXD <polegolasplays@gmail.com>
2022-05-15 17:47:43 -0300

    Initial commit

---
+++ src/StandoutVariates.java
@@ -0,0 +1,22 @@
+public class StandoutVariates {
+
+    public static void main(String[] args){
+        System.out.println("Hello new test");
+
+        int age;
+        age = 37;
+
+        System.out.println(age);
+
+        age = 30 + 10;
+
+        System.out.println(age);
+
+        age = (7 * 5) + 2;
+
+        System.out.println(age);
+
+        System.out.println("The age is: " + age + ", congratulations.");
+    }
+
+}
---
+++ src/TestingFloatingPoint.java
@@ -0,0 +1,39 @@
+public class TestingFloatingPoint {
+
+    public static void main(String[] args) {
+
+        double salary = 1250.70;
+        System.out.println("My salary is " + salary);
+
+        double age = 37;
+
+
+        double division = 3.14 / 2;
+        System.out.println(division);
+
+        int anotherDivision = 5 / 2;
+        System.out.println(anotherDivision);
+
+        double newTry = 5.0 / 2;
+        System.out.println(newTry);
+
+
+        int day =4;
+        int anotherDay = 4 + day;
+        System.out.println("A " + anotherDay);
+
+        int gifts = 15;
+        int people = 2;
+        System.out.println("B " + gifts / people);
+
+        double weight = 4.0;
+        int theAmount = 10;
+        System.out.println("C " + weight * theAmount);
+
+        double price = 5.5;
+        int tickets = 4;
+        System.out.println("D " + price * tickets);
+
+    }
+
+}
