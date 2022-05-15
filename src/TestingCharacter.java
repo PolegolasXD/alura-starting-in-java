456f10ccbfa78a8f93c2160bde14b5add05aa27e
PolegolasXD <polegolasplays@gmail.com>
2022-05-15 19:33:32 -0300

    feat: new learned with String and char.

---
+++ src/TestingCharacter.java
@@ -0,0 +1,28 @@
+import java.sql.SQLOutput;
+
+public class TestingCharacter {
+    public static void main(String[] args) {
+        //retorna :1020 <3
+        String portion1 = "10";
+        String portion2 = "20";
+        System.out.println(portion1 + portion2);
+
+
+        char letter = 'a';
+        System.out.println(letter);
+
+        char value = 66;
+        System.out.println(value);
+
+        value = (char) (value + 1);
+        System.out.println(value);
+
+        String word = "Alura cursos onlin de tecnologia ";
+        System.out.println(word);
+
+        word = word + 2020;
+        System.out.println(word);
+
+
+    }
+}
