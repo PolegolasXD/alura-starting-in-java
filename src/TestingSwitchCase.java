3c0e6549e3b42d3c6f955051c2a4d812a68ef085
PolegolasXD <polegolasplays@gmail.com>
2022-05-16 21:51:05 -0300

    feat: testing switch/case method.

---
+++ src/TestingSwitchCase.java
@@ -0,0 +1,50 @@
+public class TestingSwitchCase {
+
+    public static void main(String[] args) {
+
+        int month = 10;
+
+        switch (month) {
+            case 1:
+                System.out.println("O mês é January");
+                break;
+            case 2:
+                System.out.println("O mês é February");
+                break;
+            case 3:
+                System.out.println("O mês é March");
+                break;
+            case 4:
+                System.out.println("O mês é April");
+                break;
+            case 5:
+                System.out.println("O mês é May");
+                break;
+            case 6:
+                System.out.println("O mês é June");
+                break;
+            case 7:
+                System.out.println("O mês é July");
+                break;
+            case 8:
+                System.out.println("O mês é August");
+                break;
+            case 9:
+                System.out.println("O mês é September");
+                break;
+            case 10:
+                System.out.println("O mês é October");
+                break;
+            case 11:
+                System.out.println("O mês é November");
+                break;
+            case 12:
+                System.out.println("O mês é December");
+                break;
+            default:
+                System.out.println("invalid month");
+                break;
+        }
+    }
+
+}
